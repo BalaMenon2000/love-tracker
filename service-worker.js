545d90307a56b1c104fc9d399968e3348fa30a66
BalaMenon2000 <balarocks946@gmail.com>
2026-03-12 13:07:20 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,32 @@
+const CACHE_NAME = "ldr-tracker-v2";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/hangout.html",
+  "/workout.html",
+  "/firebase.js",
+  "/manifest.json",
+  "/icon.png"
+];
+
 self.addEventListener("install", e => {
-e.waitUntil(
-caches.open("app").then(cache=>{
-return cache.addAll([
-"/",
-"/index.html"
-])
-})
-)
-})
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
